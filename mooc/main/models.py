7eb96a08b23d5f7860bfa5aba057580d21bf380a
Dalon-ubuntu <dalonlobo@gmail.com>
2018-01-12 14:32:42 +0530

    models added

--- mooc/main/models.py
+++ mooc/main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.forms import ModelForm, Textarea
+from django.contrib import admin
 
-# Create your models here.
+class CourseDetails(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    faculty = models.CharField(max_length=100, blank=True)
+    pre_requisites = models.CharField(max_length=300, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    
+class StudentDetail(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=100, blank=True)
+    marital_status = models.CharField(max_length=100, blank=True)
+    profession = models.CharField(max_length=100, blank=True)
+    education = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=300, blank=True)
+    churned = models.BooleanField(max_length=100, blank=True)
+
+class StudentCourses(models.Model):
+    id = models.AutoField(primary_key=True)
+    student = models.ForeignKey(StudentDetail, on_delete=models.CASCADE)
+    courses = models.ForeignKey(CourseDetails, on_delete=models.CASCADE)
